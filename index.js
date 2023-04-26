4ffd85677fc908288792dcbae67dcc1da7e99505
Steven Mikhu <StevenMikhu@gmail.com>
2023-04-25 22:41:00 -0400

    emitted a log message with a unique ID

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("testing!");
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+// init object
+
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  //emit the event
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
